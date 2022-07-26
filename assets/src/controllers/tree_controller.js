825a0af042d2d0993076e2d0f430ac92ef86b3e5
Tac Tacelosky <tacman@gmail.com>
2022-07-26 19:05:04 -0400

    improve controllers

--- assets/src/controllers/tree_controller.js
+++ assets/src/controllers/tree_controller.js
@@ -7,24 +7,36 @@ import 'jstree';
 export default class extends Controller {
 
     static values = {
-        msg: { type: String, default: '/bill' },
+        msg: {type: String, default: '/bill'},
+        plugins: {type: Array, default: ['checkbox', 'theme', "types", 'sort']},
+        types: {type: Object, default: {}}
         // interval: { type: Number, default: 5 },
         // clicked: { type: Boolean, default: false },
     }
 
-    static targets = [ "html", "ajax" ]
+    static targets = ["html", "ajax"]
 
     connect() {
         console.log('hello from ' + this.identifier);
         // this.element.textContent = msg;
         if (this.hasHtmlTarget) {
             this.html(this.htmlTarget);
+        } else {
+            console.error('Warning: no HTML target, so not rendered.');
         }
     }
 
-    addListeners($element) {
-        console.log('adding listeners. ', $element);
-        $element
+    search(event) {
+        // this.$element.jstree(true).search(event.currentTarget.value, {
+        //     show_only_matches: true,
+        //     show_only_matches_children: true
+        // });
+        this.$element.jstree(true).search(event.currentTarget.value, false, true, true);
+    }
+
+    addListeners() {
+        console.log('adding listeners. ');
+        this.$element
             .on('changed.jstree', this.onChanged) // triggered when selection changes, can be multiple, data is tree data, not node data
             .on('ready.jstree', (e, data) => {
                 console.warn('ready.jstree fired, so opening_all');
@@ -35,13 +47,18 @@ export default class extends Controller {
 
     html(el) {
         // jQuery.tree.reference(el );
-        let $el = $(el);
-        $el.jstree(
+        this.$element = $(el);
+        // this.$element = jQuery.jstree.reference(el);
+        console.error(this.$element);
+        console.error(this.pluginsValue);
+        this.$element.jstree(
             {
-                "plugins": ['checkbox', 'theme', "html_data", "types"]
+                "plugins": this.pluginsValue,
+                "types": this.typesValue
             }
         );
-        this.addListeners($el);
+
+        this.addListeners();
 
     }
 }
