6dd8e2182b7b116264c9274995bf9781ff5237a9
Tac Tacelosky <tacman@gmail.com>
2026-02-11 16:04:45 -0500

    refactor jstree runtime

--- assets/src/jstree_runtime.js
+++ assets/src/jstree_runtime.js
@@ -1,4 +1,13 @@
 import $ from 'jquery';
+import {
+    createTree,
+    getTree,
+    hasTree,
+    callTree,
+    destroyTree,
+    configureDefaults,
+    version
+} from '@tacman1123/jstree-esm';
 
 if (!window.jQuery) {
     window.jQuery = $;
@@ -7,14 +16,4 @@ if (!window.$) {
     window.$ = $;
 }
 
-await import('@tacman1123/jstree-esm/jquery-plugin');
-
-export {
-    createTree,
-    getTree,
-    hasTree,
-    callTree,
-    destroyTree,
-    configureDefaults,
-    version
-} from '@tacman1123/jstree-esm/browser-module';
+export { createTree, getTree, hasTree, callTree, destroyTree, configureDefaults, version };
